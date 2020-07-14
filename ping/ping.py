d70d08ae55ba0e35755c8502e48693f943673697
maxbooiii <63972751+maxbooiii@users.noreply.github.com>
2020-07-14 16:03:40 +0200

    Improved, `ping t` now shows with shards.
    # no longer needed to edit the cog to actually have shards on the main, while alot dont prefer shards until they've got 2.

--- ping/ping.py
+++ ping/ping.py
@@ -2,6 +2,7 @@ import time
 import discord
 
 from redbot.core import checks, Config, commands
+from redbot.core.utils import chat_formatting as chat
 
 class Ping(commands.Cog):
     """Reply with latency of bot"""
@@ -13,8 +14,8 @@ class Ping(commands.Cog):
 
     @commands.cooldown(rate=1, per=3, type=commands.BucketType.user)
     @commands.bot_has_permissions(embed_links=True)
-    @commands.command(aliases=["pinf", "pingt", "pin"])
-    async def ping(self, ctx,):
+    @commands.group(invoke_without_command=True)
+    async def ping(self, ctx):
         """Reply with latency of bot"""
         latency = self.bot.latency * 1000
         emb = discord.Embed(title="Ping!\N{TABLE TENNIS PADDLE AND BALL}", color=discord.Color.red())
@@ -25,10 +26,36 @@ class Ping(commands.Cog):
         message = await ctx.send(embed=emb)
         ping = (time.monotonic() - before) * 1000
 
+        # french people aint that bad, right?, Thanks preda for adding shards.
         # shards = [f"Shard {shard + 1}/{self.bot.shard_count}: {round(pingt * 1000)} ms\n" for shard, pingt in self.bot.latencies]
         emb = discord.Embed(title="Ping!\N{TABLE TENNIS PADDLE AND BALL}", color=discord.Color.green())
         emb.add_field(name="Discord API:", inline=False, value=(str(round(latency)) + " ms"))
         emb.add_field(name="Typing:", value=(str(round(ping)) + " ms"))
         # emb.add_field(name="Shards:", value=("".join(shards)), inline=False)
+        await message.edit(embed=emb)
 
+    @ping.command()
+    async def t(self, ctx):
+        """Reply with latency of bot."""
+        latency = self.bot.latency * 1000
+        emb = discord.Embed(title="Ping!\N{TABLE TENNIS PADDLE AND BALL}", color= await ctx.embed_color())
+        emb.add_field(
+            name="Discord API:", value=(str(round(latency)) + " ms"),
+        )
+
+        before = time.monotonic()
+        message = await ctx.send(embed=emb)
+        ping = (time.monotonic() - before) * 1000
+        shards = [f"Shard {shard + 1}/{self.bot.shard_count}: {round(pingt * 1000)} ms\n" for shard, pingt in self.bot.latencies]
+        # Thanks preda, but i copied this from fixator10 and fixator have improved it good. :D
+        if len(self.bot.latencies) > 1:
+            # The chances of this in near future is almost 0, but who knows, what future will bring to us?
+            shards = [
+                f"Shard {shard + 1}/{self.bot.shard_count}: {round(pingt * 1000)}ms"
+                for shard, pingt in self.bot.latencies
+            ]
+            emb.add_field(name="Shards:", value=("\n".join(shards)))
+        emb.colour =  await ctx.embed_color()
+        emb.add_field(name="Typing:", value=(str(round(ping)) + " ms"))
+        emb.add_field(name="Shards:", value=("".join(shards)), inline=False)
         await message.edit(embed=emb)
