711277b78eeb01fe4cd1ccf279889b8ac13b4717
maxbooiii <63972751+maxbooiii@users.noreply.github.com>
2020-07-25 23:48:37 +0200

    Data API

--- pokeseries/__init__.py
+++ pokeseries/__init__.py
@@ -1,4 +1,8 @@
 from .pokeseries import PokeSeries
 
+__red_end_user_data_statement__ = (
+    "This cog does not persistently store data or metadata about users."
+)
+
 def setup(bot):
     bot.add_cog(PokeSeries(bot))
