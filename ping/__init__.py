deb567b17fef48708b847a8071b34fb16cae03fd
maxbooiii <63972751+maxbooiii@users.noreply.github.com>
2020-07-25 23:48:59 +0200

    Data API

--- ping/__init__.py
+++ ping/__init__.py
@@ -1,5 +1,9 @@
 from .ping import Ping
 
+__red_end_user_data_statement__ = (
+    "This cog does not persistently store data or metadata about users."
+)
+
 def setup(bot):
     n = Ping(bot)
     bot.add_cog(n)
