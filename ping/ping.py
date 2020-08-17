fbe1ba97143eb19c79e3a20f5623837bebeb9635
maxbooiii <63972751+maxbooiii@users.noreply.github.com>
2020-08-17 11:52:04 -0700

    [ping] fixed what people actually asked for.
    * you can still not get the core ping to show on unload... i still dont know how to fix this yet, so you may restart your bot if you want the orginal.
    * feel free to pr it if you know how.

--- ping/ping.py
+++ ping/ping.py
@@ -15,50 +15,39 @@ class Ping(commands.Cog):
         self.bot = bot
         self.bot.remove_command("ping")
 
+    @commands.command(aliases=["pingtime", "pingt", "pig", "pnig", "pung", "pinf"])
+    @commands.has_permissions(embed_links=True)
     @commands.cooldown(rate=1, per=3, type=commands.BucketType.user)
-    @commands.bot_has_permissions(embed_links=True)
-    @commands.group(invoke_without_command=True)
     async def ping(self, ctx):
-        """Reply with latency of bot"""
-        latency = self.bot.latency * 1000
-        emb = discord.Embed(title="Ping!\N{TABLE TENNIS PADDLE AND BALL}", color=discord.Color.red())
-        emb.add_field(name="Discord API:", inline=False, value=(str(round(latency)) + " ms"))
-        emb.add_field(name="Typing:", value=("calculating" + " ms"))
-
-        before = time.monotonic()
-        message = await ctx.send(embed=emb)
-        ping = (time.monotonic() - before) * 1000
-
-        # french people aint that bad, right?, Thanks preda for adding shards.
-        # shards = [f"Shard {shard + 1}/{self.bot.shard_count}: {round(pingt * 1000)} ms\n" for shard, pingt in self.bot.latencies]
-        emb = discord.Embed(title="Ping!\N{TABLE TENNIS PADDLE AND BALL}", color=discord.Color.green())
-        emb.add_field(name="Discord API:", inline=False, value=(str(round(latency)) + " ms"))
-        emb.add_field(name="Typing:", value=(str(round(ping)) + " ms"))
-        # emb.add_field(name="Shards:", value=("".join(shards)), inline=False)
-        await message.edit(embed=emb)
-
-    @ping.command()
-    async def t(self, ctx):
         """Reply with latency of bot."""
         latency = self.bot.latency * 1000
-        emb = discord.Embed(title="Ping!\N{TABLE TENNIS PADDLE AND BALL}", color= await ctx.embed_color())
+        emb = discord.Embed(title="Pong!", color=discord.Color.red())
         emb.add_field(
-            name="Discord API:", value=(str(round(latency)) + " ms"),
+            name="Discord API", value=chat.box(str(round(latency)) + " ms"),
         )
-
-        before = time.monotonic()
-        message = await ctx.send(embed=emb)
-        ping = (time.monotonic() - before) * 1000
-        shards = [f"Shard {shard + 1}/{self.bot.shard_count}: {round(pingt * 1000)} ms\n" for shard, pingt in self.bot.latencies]
-        # Thanks preda, but i copied this from fixator10 and fixator have improved it good. :D
+        emb.add_field(name="Message", value=chat.box("…"))
+        emb.add_field(name="Typing", value=chat.box("…"))
+        # Thanks preda and fixator for the good improvements done with this command.
         if len(self.bot.latencies) > 1:
             # The chances of this in near future is almost 0, but who knows, what future will bring to us?
             shards = [
                 f"Shard {shard + 1}/{self.bot.shard_count}: {round(pingt * 1000)}ms"
                 for shard, pingt in self.bot.latencies
             ]
-            emb.add_field(name="Shards:", value=("\n".join(shards)))
-        emb.colour =  await ctx.embed_color()
-        emb.add_field(name="Typing:", value=(str(round(ping)) + " ms"))
-        emb.add_field(name="Shards:", value=("".join(shards)), inline=False)
+            emb.add_field(name="Shards", value=chat.box("\n".join(shards)))
+        # in future this may be removed or changed with improvements, if you wanna keep it as it is, pin it in your cog list.
+        before = time.monotonic()
+        message = await ctx.send(embed=emb)
+        ping = (time.monotonic() - before) * 1000
+        emb.colour = await ctx.embed_color()
+        emb.set_field_at(
+            1,
+            name="Message",
+            value=chat.box(
+                str(int((message.created_at - ctx.message.created_at).total_seconds() * 1000))
+                + " ms"
+            ),
+        )
+        emb.set_field_at(2, name="Typing", value=chat.box(str(round(ping)) + " ms"))
+
         await message.edit(embed=emb)
