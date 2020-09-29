c34586ed8b53325b1da6c31c56f2496d3f601cf4
maxbooiii <63972751+maxbooiii@users.noreply.github.com>
2020-09-29 08:27:43 +0200

    forgot an `:`

--- ping/ping.py
+++ ping/ping.py
@@ -45,7 +45,7 @@ class Ping(commands.Cog):
         latency = self.bot.latency * 1000
         emb = discord.Embed(title="Pong!", color=discord.Color.red())
         emb.add_field(
-            name="Discord WS", value=chat.box(str(round(latency)) + " ms"),
+            name="Discord WS:", value=chat.box(str(round(latency)) + " ms"),
         )
         emb.add_field(name="Message:", value=chat.box("…"))
         emb.add_field(name="Typing:", value=chat.box("…"))
