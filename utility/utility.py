1e36949f71855a4b014036bd5d30ec4cd47319ee
MAX <63972751+maxbooiii@users.noreply.github.com>
2020-11-20 13:24:48 +0100

    [Utility]Added an hidden command thats meant for bigger bots only.
    It will shows shards below it all.

--- utility/utility.py
+++ utility/utility.py
@@ -31,9 +31,9 @@ class Utility(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.cooldown(rate=1, per=60, type=commands.BucketType.user)
+    @commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
     @commands.has_permissions(embed_links=True)
-    @commands.command()
+    @commands.group(invoke_without_command=True)
     async def statsinfo(self,ctx):
         """This shows some botstats."""
         total_members = 0
@@ -59,7 +59,42 @@ class Utility(commands.Cog):
         emb.add_field(name="Ping:", value=chat.box(str(round(latency)) + " ms"))
         emb.add_field(name="Servers:", value=chat.box(servers))
         emb.add_field(name='Members:', inline=False, value=chat.box(f'{total_members} total\n{total_unique} unique'))
-        emb.add_field(name='Channels:', value=chat.box(f'{text + voice} total\n{text} text\n{voice} voice'))
+        emb.add_field(name='Channels:', inline=False, value=chat.box(f'{text + voice} total\n{text} text\n{voice} voice'))
+        emb.set_footer(text=f'discord.py v{version}')
+        emb.timestamp = datetime.datetime.utcnow()
+        await ctx.send(embed=emb)
+
+    @commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
+    @commands.has_permissions(embed_links=True)
+    @statsinfo.command(hidden=True)
+    async def t(self,ctx):
+        """Shows some more stats."""
+        total_members = 0
+        total_unique = len(self.bot.users)
+
+        text = 0
+        voice = 0
+        guilds = 0
+        for guild in self.bot.guilds:
+            guilds += 1
+            total_members += guild.member_count
+            for channel in guild.channels:
+                if isinstance(channel, discord.TextChannel):
+                    text += 1
+                elif isinstance(channel, discord.VoiceChannel):
+                    voice += 1
+
+        shards = [f"Shard {shard + 1}/{self.bot.shard_count}: {round(pingt * 1000)}ms\n" for shard, pingt in self.bot.latencies]
+        latency = self.bot.latency * 1000
+        version = pkg_resources.get_distribution('discord.py').version
+        servers = str(len(self.bot.guilds))
+        users = str(len(self.bot.users))
+        emb = discord.Embed(title=f"Botstats of {self.bot.user.name}:", color=discord.Color.green())
+        emb.add_field(name="Ping:", value=chat.box(str(round(latency)) + " ms"))
+        emb.add_field(name="Servers:", value=chat.box(servers))
+        emb.add_field(name='Members:', inline=False, value=chat.box(f'{total_members} total\n{total_unique} unique'))
+        emb.add_field(name='Channels:', inline=False, value=chat.box(f'{text + voice} total\n{text} text\n{voice} voice'))
+        emb.add_field(name="Shards:", value=box("".join(shards)))
         emb.set_footer(text=f'discord.py v{version}')
         emb.timestamp = datetime.datetime.utcnow()
         await ctx.send(embed=emb)
