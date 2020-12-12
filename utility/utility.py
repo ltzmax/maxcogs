771d7f0e16e2036356db3a3485b6b248013b7aea
MAX <63972751+maxbooiii@users.noreply.github.com>
2020-12-13 00:52:18 +0100

    [Utility] Fixes and added an `jointime` command.
    * this is just for lazyness. lmao

--- utility/utility.py
+++ utility/utility.py
@@ -1,9 +1,11 @@
 import discord
 import datetime
+import time
 import psutil
 import humanize
-from tabulate import tabulate
 import pkg_resources
+
+from tabulate import tabulate
 from lavalink import node
 from redbot.core import commands
 from redbot.core.utils import chat_formatting as chat
@@ -38,6 +40,11 @@ class Utility(commands.Cog):
         """This shows some botstats."""
         total_members = 0
         total_unique = len(self.bot.users)
+
+        mem_v = psutil.virtual_memory()
+        memoryused = self._size(mem_v.total - mem_v.available)
+        memorytotal = self._size(mem_v.total)
+        memorypercent = mem_v.percent
 
         mem_v = psutil.virtual_memory()
         memoryused = self._size(mem_v.total - mem_v.available)
@@ -108,3 +115,52 @@ class Utility(commands.Cog):
                 )
             )
         await menu(ctx, tabs, DEFAULT_CONTROLS)
+
+    @commands.command()
+    @commands.guild_only()
+    @commands.bot_has_permissions(embed_links=True)
+    async def jointime(self, ctx, *, user: discord.Member = None):
+        """Show's just join server and discord join time.
+        
+        It also shows in [p]userinfo. This is just the lazy way to check jointime for a user, 
+        if you really don't want to check anything else."""
+
+        async with ctx.typing():
+            author = ctx.author
+            guild = ctx.guild
+
+            if not user:
+                user = author
+
+            joined_at = user.joined_at
+            since_created = int((ctx.message.created_at - user.created_at).days)
+            if joined_at is not None:
+                since_joined = int((ctx.message.created_at - joined_at).days)
+                user_joined = joined_at.strftime("%d %b %Y %H:%M")
+            else:
+                since_joined = "?"
+                user_joined = "Unknown"
+            user_created = user.created_at.strftime("%d %b %Y %H:%M")
+            member_number = (
+                sorted(guild.members, key=lambda m: m.joined_at or ctx.message.created_at).index(
+                    user
+                )
+                + 1
+            )
+
+            created_on = "`{}` ({} day{} ago)".format(
+                user_created, since_created, "" if since_created == 1 else "s"
+            )
+            joined_on = "`{}` ({} day{} ago)".format(
+                user_joined, since_joined, "" if since_joined == 1 else "s"
+            )
+
+            em = discord.Embed(title="Semi userinfo:", colour=(await ctx.embed_colour()))
+            em.add_field(name="Joined Discord:", inline=False, value=created_on)
+            em.add_field(name="Joined Server:", value=joined_on)
+
+        try:
+            await ctx.send(embed=em)
+        except discord.HTTPException:
+            await ctx.send("I need the `Embed links` permission "
+                               "to send this")
