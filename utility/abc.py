e91c4dfffc307a80244516e0680db1480aac5f82
MAX <63972751+maxbooiii@users.noreply.github.com>
2020-12-14 19:42:10 +0100

    [utility] Split stats into its own file.

---
+++ utility/abc.py
@@ -0,0 +1,15 @@
+from abc import ABC
+
+import discord
+from redbot.core import commands
+from redbot.core.bot import Red
+
+
+class MixinMeta(ABC):
+    """
+    Base class for well behaved type hint detection with composite class.
+    Basically, to keep developers sane when not all attributes are defined in each mixin.
+    """
+
+    def __init__(self, *_args):
+        self.bot: Red
--- utility/utility.py
+++ utility/utility.py
@@ -1,9 +1,9 @@
 import discord
-import datetime
 import time
-import psutil
 import humanize
-import pkg_resources
+
+from abc import ABC
+from .stats import Stats
 
 from tabulate import tabulate
 from lavalink import node
@@ -24,8 +24,18 @@ async def parse_llnode_stat(stats: node.NodeStats, stat_name: str):
         stat = f"{round(stat*100, 2)} %"
     return stat
 
-class Utility(commands.Cog):
-    """Random utility will be in this cog."""
+class CompositeMetaClass(type(commands.Cog), type(ABC)):
+    """
+    This allows the metaclass used for proper type detection to
+    coexist with discord.py's metaclass
+    """
+
+class Utility(
+    Stats,
+    commands.Cog,
+    metaclass=CompositeMetaClass,
+):
+    """utility."""
 
     async def red_delete_data_for_user(self, **kwargs):
         """Nothing to delete."""
@@ -33,55 +43,6 @@ class Utility(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-
-    @commands.has_permissions(embed_links=True)
-    @commands.command()
-    async def statsinfo(self,ctx):
-        """This shows some botstats."""
-        total_members = 0
-        total_unique = len(self.bot.users)
-
-        mem_v = psutil.virtual_memory()
-        memoryused = self._size(mem_v.total - mem_v.available)
-        memorytotal = self._size(mem_v.total)
-        memorypercent = mem_v.percent
-
-        net_io = psutil.net_io_counters()
-        bytes_recv = self._size(net_io.bytes_recv)
-        bytes_sent = self._size(net_io.bytes_sent)
-
-        text = 0
-        voice = 0
-        guilds = 0
-        for guild in self.bot.guilds:
-            guilds += 1
-            total_members += guild.member_count
-            for channel in guild.channels:
-                if isinstance(channel, discord.TextChannel):
-                    text += 1
-                elif isinstance(channel, discord.VoiceChannel):
-                    voice += 1
-
-        version = pkg_resources.get_distribution('discord.py').version
-        servers = str(len(self.bot.guilds))
-        users = str(len(self.bot.users))
-        emb = discord.Embed(title=f"Botstats of {self.bot.user.name}:", color=discord.Color.green())
-        emb.add_field(name="Users:", value=(f'{servers} servers\n{total_members} total members\n{total_unique} unique members'), inline=True)
-        emb.add_field(name="Channels:", value=(f'{text + voice} total\n{text} text\n{voice} voice'), inline=True)
-        emb.add_field(name="\N{ZERO WIDTH SPACE}", value="\N{ZERO WIDTH SPACE}", inline=False)
-        emb.add_field(name="Memory:", value=("{} / {}".format(memoryused, memorytotal)), inline=True)
-        emb.add_field(name="Network traffic:", value=('Sent: {}\nReceived: {}'.format(bytes_sent, bytes_recv)), inline=True)
-        emb.set_footer(text=f'discord.py v{version}')
-        emb.timestamp = datetime.datetime.utcnow()
-        await ctx.send(embed=emb)
-
-    @staticmethod
-    def _size(num):
-        for unit in ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB"]:
-            if abs(num) < 1024.0:
-                return "{0:.1f}{1}".format(num, unit)
-            num /= 1024.0
-        return "{0:.1f}{1}".format(num, "YB")
 
     @commands.command()
     @commands.is_owner()
