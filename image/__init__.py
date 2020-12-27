378a27edd858cd83718d2efc8a10019190200993
MAX <63972751+maxbooiii@users.noreply.github.com>
2020-12-27 19:37:59 +0100

    [image] new image cog, who dis?

---
+++ image/__init__.py
@@ -0,0 +1,8 @@
+from .image import Image
+
+__red_end_user_data_statement__ = (
+    "This cog does not persistently store data or metadata about users."
+)
+
+def setup(bot):
+    bot.add_cog(Image(bot))
