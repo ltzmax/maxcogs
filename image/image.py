f420e247957f1cd0b90542f35b62c603677c50ca
MAX <63972751+maxbooiii@users.noreply.github.com>
2021-01-03 23:15:27 +0100

    Remove food command due to several nsfw post.
    * i'll find something else in a later date.

--- image/image.py
+++ image/image.py
@@ -61,26 +61,3 @@ class Image(commands.Cog):
             await ctx.send(embed=embed)
         except discord.HTTPException:
             await ctx.send("Bad reponse, please retry the command again.")
-            
-    @commands.command()
-    @commands.guild_only()
-    @commands.cooldown(1, 3, commands.BucketType.guild)
-    @commands.max_concurrency(1, commands.BucketType.guild)
-    @commands.bot_has_permissions(embed_links=True)
-    async def food(self, ctx):
-        """Send a random food image."""
-        async with aiohttp.ClientSession() as session:
-            async with session.get("https://api.martinebot.com/v1/images/subreddit?name=food") as resp:
-                response = await resp.json()
-            embed = await ctx.embed_colour()
-            embed = discord.Embed(
-                title = response["data"].get("title", "[No Title]"),
-                url = response["data"]["post_url"],
-                color = embed
-            )
-            embed.set_footer(text=f"Powered by martinebot.com API")
-            embed.set_image(url=response["data"]["image_url"])
-        try:
-            await ctx.send(embed=embed)
-        except discord.HTTPException:
-            await ctx.send("Bad reponse, please retry the command again.")
