e63dabbdf6e645ea7659b10054cfed35dfef565c
MAX <63972751+maxbooiii@users.noreply.github.com>
2021-01-06 18:53:44 +0100

    [fun]Allow the commands to work wihtout embed perm

--- fun/fun.py
+++ fun/fun.py
@@ -16,23 +16,27 @@ class Fun(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    @commands.bot_has_permissions(embed_links=True)
     async def f(self, ctx, *, text: commands.clean_content = None):
         """Press F to pay respect."""
         reason = f"for {text} " if text else ""
-        emb = discord.Embed(
-            color=0x30BA8F,
-            description=(f"**{ctx.author.name}** has paid their respect {reason}")
-        )
-        await ctx.send(embed=emb)
+        if ctx.channel.permissions_for(ctx.me).embed_links:
+            emb = discord.Embed(
+                description=(f"**{ctx.author.name}** has paid their respect {reason}"),
+                color=discord.Color.green(),
+            )
+            await ctx.send(embed=emb)
+        else:
+            await ctx.send(f"**{ctx.author.name}** has paid their respect {reason}")
 
     @commands.command()
-    @commands.bot_has_permissions(embed_links=True)
     async def heart(self, ctx, *, text: commands.clean_content = None):
         """Give someone a heart."""
         hearts = ['\N{HEAVY BLACK HEART}\N{VARIATION SELECTOR-16}', '\N{BLUE HEART}', '\N{BROWN HEART}', '\N{PURPLE HEART}', '\N{GREEN HEART}', '\N{WHITE HEART}', '\N{YELLOW HEART}', '\N{BLACK HEART SUIT}\N{VARIATION SELECTOR-16}', '\N{GROWING HEART}', '\N{BEATING HEART}', '\N{TWO HEARTS}', '\N{SPARKLING HEART}', '\N{REVOLVING HEARTS}']
-        emb = discord.Embed(
-            color=0x30BA8F,
-            description=(f"**{ctx.author.name}** has given you a heart {random.choice(hearts)}")
-        )
-        await ctx.send(embed=emb)
+        if ctx.channel.permissions_for(ctx.me).embed_links:
+            emb = discord.Embed(
+                description=(f"**{ctx.author.name}** has given you a heart {random.choice(hearts)}"),
+                color=discord.Color.green(),
+            )
+            await ctx.send(embed=emb)
+        else:
+            await ctx.send(f"**{ctx.author.name}** has given you a heart {random.choice(hearts)}")
