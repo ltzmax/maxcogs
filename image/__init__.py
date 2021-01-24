769ae48501624203fa0a69193a24dfc3197a5af3
maxbooiii <maxbooiii@outlook.com>
2021-01-24 13:47:23 +0100

    [image] New command added.
    * [p]anime command is now added, please note that this does not suit for everyone.

--- image/__init__.py
+++ image/__init__.py
@@ -4,5 +4,6 @@ __red_end_user_data_statement__ = (
     "This cog does not persistently store data or metadata about users."
 )
 
+
 def setup(bot):
     bot.add_cog(Image(bot))
--- image/image.py
+++ image/image.py
@@ -5,7 +5,7 @@ from redbot.core import commands
 
 
 class Image(commands.Cog):
-    """Random images to look at.
+    """Random image cog posted from reddit.
 
     Powered by martinebot.com API."""
 
@@ -17,10 +17,52 @@ class Image(commands.Cog):
         """Nothing to delete."""
         return
 
+    # Credits to Predä. for this snippet. but i snipped it from owo-cogs.
+    async def _nsfw_check(self, ctx: commands.Context):
+        """Message for Safe For Work (SFW) channels."""
+        if not ctx.message.channel.is_nsfw():
+            em = discord.Embed(
+                title="\N{LOCK} NSFW commands Cannot be used in non NSFW channel.",
+                color=0xAA0000,
+            )
+        return em
+
+    @commands.command()
     @commands.cooldown(1, 3, commands.BucketType.guild)
     @commands.max_concurrency(1, commands.BucketType.guild)
     @commands.bot_has_permissions(embed_links=True)
+    async def anime(self, ctx):
+        """Random anime photos.
+
+        This is marked as nsfw due to some images can be nsfw.
+        """
+        if not ctx.message.channel.is_nsfw():
+            await ctx.send(embed=await self._nsfw_check(ctx))
+            return
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                "https://api.martinebot.com/v1/images/subreddit?name=anime_irl"
+            ) as resp:
+                response = await resp.json()
+
+            embed = await ctx.embed_colour()
+            embed = discord.Embed(
+                title=response["data"].get("title", "[No Title]"),
+                url=response["data"]["post_url"],
+                color=embed,
+            )
+            embed.set_footer(text=f"Powered by martinebot.com API")
+            embed.set_image(url=response["data"]["image_url"])
+        try:
+            await ctx.send(embed=embed)
+        except discord.HTTPException:
+            await ctx.send("Bad reponse, please retry the command again.")
+
     @commands.command()
+    @commands.cooldown(1, 3, commands.BucketType.guild)
+    @commands.max_concurrency(1, commands.BucketType.guild)
+    @commands.bot_has_permissions(embed_links=True)
     async def earth(self, ctx):
         """Random earth photos."""
         async with aiohttp.ClientSession() as session:
@@ -41,10 +83,10 @@ class Image(commands.Cog):
         except discord.HTTPException:
             await ctx.send("Bad reponse, please retry the command again.")
 
+    @commands.command()
     @commands.cooldown(1, 3, commands.BucketType.guild)
     @commands.max_concurrency(1, commands.BucketType.guild)
     @commands.bot_has_permissions(embed_links=True)
-    @commands.command()
     async def space(self, ctx):
         """Random space photos."""
         async with aiohttp.ClientSession() as session:
