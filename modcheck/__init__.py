2d8e9fa700c14720d25b83423969d25a67a31308
MAX <63972751+maxbooiii@users.noreply.github.com>
2021-01-27 20:16:58 +0100

    Delete modcheck directory

--- modcheck/__init__.py
+++
@@ -1,8 +0,0 @@
-from .modcheck import ModCheck
-
-__red_end_user_data_statement__ = (
-    "This cog does not persistently store data or metadata about users."
-)
-
-def setup(bot):
-    bot.add_cog(ModCheck(bot))
