6276b9709215d68518fea081c599c456d852070a
maxbooiii <maxbooiii@outlook.com>
2021-01-27 19:40:49 +0100

    Remove checkabuse.
    * there was an issue with it so it wont be here again.

--- utility/core.py
+++ utility/core.py
@@ -8,7 +8,6 @@ import pkg_resources
 from tabulate import tabulate
 from lavalink import node
 from redbot.core import commands
-from collections import defaultdict
 from redbot.core.utils import chat_formatting as chat
 from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
 from redbot.core.utils.chat_formatting import (
@@ -30,7 +29,7 @@ async def parse_llnode_stat(stats: node.NodeStats, stat_name: str):
 class Utility(commands.Cog):
     """Utility commands to use."""
 
-    __version__ = "1.3.0"
+    __version__ = "1.4.0"
     __author__ = ["MAX", "Fixator10"]
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
@@ -108,38 +107,6 @@ class Utility(commands.Cog):
                 return "{0:.1f}{1}".format(num, unit)
             num /= 1024.0
         return "{0:.1f}{1}".format(num, "YB")
-
-    @commands.is_owner()
-    @commands.command()
-    async def checkabuse(self, ctx):
-        """This checks servers for spam."""
-        c = defaultdict(int)
-        u = defaultdict(int)
-        me = defaultdict(int)
-        me_u = defaultdict(int)
-        for m in ctx.guild._state._messages:
-            me[m.guild] += 1
-            me_u[m.author] += 1
-            if ctx.valid:
-                c[
-                    (
-                        m.guild,
-                        getattr(m.guild, "owner", m.author),
-                        ctx.command.qualified_name,
-                    )
-                ] += 1
-                u[(m.author, ctx.command.qualified_name)] += 1
-        c_offender = max(c.items(), key=lambda kv: kv[1])
-        u_offender = max(u.items(), key=lambda kv: kv[1])
-
-        me_offender = max(me.items(), key=lambda kv: kv[1])
-        me_u_offender = max(me_u.items(), key=lambda kv: kv[1])
-        await ctx.send(
-            box(
-                f"\nServer with most Message:\n{me_offender}\n\nUser with most messages:\n{me_u_offender}\n\n\nServer with most commands:\n{c_offender}\n\nUser with most commands:\n{u_offender}",
-                lang="py",
-            )
-        )
 
     @commands.command()
     @commands.is_owner()
