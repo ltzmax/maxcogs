18a410e02fb13e228a8bf8e2eee730f0af4dd940
maxbooiii <maxbooiii@outlook.com>
2021-01-31 15:52:57 +0100

    Add animewallpaper and animephonewallpaper

--- anime/anime.py
+++ anime/anime.py
@@ -59,6 +59,64 @@ class Anime(commands.Cog):
         except discord.HTTPException:
             await ctx.send("Bad reponse, please retry the command again.")
 
+    @commands.command(aliases=["animewallp"])
+    @commands.cooldown(1, 3, commands.BucketType.guild)
+    @commands.max_concurrency(1, commands.BucketType.guild)
+    @commands.bot_has_permissions(embed_links=True)
+    async def animewallpaper(self, ctx):
+        """Random animewallpaper images."""
+        if not ctx.message.channel.is_nsfw():
+            await ctx.send(embed=await self._nsfw_check(ctx))
+            return
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                "https://api.martinebot.com/v1/images/subreddit?name=Animewallpaper"
+            ) as resp:
+                response = await resp.json()
+
+            embed = await ctx.embed_colour()
+            embed = discord.Embed(
+                title=response["data"].get("title", "[No Title]"),
+                url=response["data"]["post_url"],
+                color=embed,
+            )
+            embed.set_footer(text=f"Powered by martinebot.com API")
+            embed.set_image(url=response["data"]["image_url"])
+        try:
+            await ctx.send(embed=embed)
+        except discord.HTTPException:
+            await ctx.send("Bad reponse, please retry the command again.")
+
+    @commands.command(aliases=["animephonewallp", "animephone"])
+    @commands.cooldown(1, 3, commands.BucketType.guild)
+    @commands.max_concurrency(1, commands.BucketType.guild)
+    @commands.bot_has_permissions(embed_links=True)
+    async def animephonewallpaper(self, ctx):
+        """Random animephonewallpaper images."""
+        if not ctx.message.channel.is_nsfw():
+            await ctx.send(embed=await self._nsfw_check(ctx))
+            return
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(
+                "https://api.martinebot.com/v1/images/subreddit?name=AnimePhoneWallpapers"
+            ) as resp:
+                response = await resp.json()
+
+            embed = await ctx.embed_colour()
+            embed = discord.Embed(
+                title=response["data"].get("title", "[No Title]"),
+                url=response["data"]["post_url"],
+                color=embed,
+            )
+            embed.set_footer(text=f"Powered by martinebot.com API")
+            embed.set_image(url=response["data"]["image_url"])
+        try:
+            await ctx.send(embed=embed)
+        except discord.HTTPException:
+            await ctx.send("Bad reponse, please retry the command again.")
+
     @commands.command(aliases=["animegirls"])
     @commands.cooldown(1, 3, commands.BucketType.guild)
     @commands.max_concurrency(1, commands.BucketType.guild)
