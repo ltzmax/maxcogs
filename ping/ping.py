af449ab3a47529f87614273bc2f38ddae63f9575
maxbooiii <maxbooiii@outlook.com>
2021-02-27 20:58:41 +0100

    Ping now support non embeds and added better install messages.

--- ping/ping.py
+++ ping/ping.py
@@ -15,7 +15,7 @@ old_ping = None
 
 class Ping(commands.Cog):
     """Reply with latency of [botname].
-    
+
     This does not matter anything as long as your ping is not above 300ms."""
 
     async def red_delete_data_for_user(self, **kwargs):
@@ -35,7 +35,6 @@ class Ping(commands.Cog):
             self.bot.add_command(old_ping)
 
     @commands.command()
-    @commands.bot_has_permissions(embed_links=True)
     async def ping(self, ctx):
         """Reply with latency of [botname].
 
@@ -44,41 +43,52 @@ class Ping(commands.Cog):
         - Discord WS: Websocket latency.
         - Message: Difference between your command and message.
         - Time: Time it takes for the bot to send message."""
-        latency = self.bot.latency * 1000
-        emb = discord.Embed(title="Pong !", color=discord.Color.red())
-        emb.add_field(
-            name="Discord WS:",
-            value=box(str(round(latency)) + " ms", "yaml"),
-        )
-        emb.add_field(name="Message:", value=box("…"))
-        emb.add_field(name="Typing:", value=box("…"))
-        if len(self.bot.latencies) > 1:
-            shards = [
-                f"Shard {shard + 1}/{self.bot.shard_count}: {round(pingt * 1000)}ms"
-                for shard, pingt in self.bot.latencies
-            ]
-            emb.add_field(name="Shards:", value=("\n".join(shards)))
+        if ctx.channel.permissions_for(ctx.me).embed_links:
+            latency = self.bot.latency * 1000
+            emb = discord.Embed(title="Pong !", color=discord.Color.red())
+            emb.add_field(
+                name="Discord WS:",
+                value=box(str(round(latency)) + " ms", "yaml"),
+            )
+            emb.add_field(name="Message:", value=box("…"))
+            emb.add_field(name="Typing:", value=box("…"))
+            if len(self.bot.latencies) > 1:
+                shards = [
+                    f"Shard {shard + 1}/{self.bot.shard_count}: {round(pingt * 1000)}ms"
+                    for shard, pingt in self.bot.latencies
+                ]
+                emb.add_field(name="Shards:", value=("\n".join(shards)))
 
-        before = time.monotonic()
-        message = await ctx.send(embed=emb)
-        ping = (time.monotonic() - before) * 1000
-        emb.colour = await ctx.embed_color()
-        emb.set_field_at(
-            1,
-            name="Message:",
-            value=box(
-                str(
-                    int(
-                        (message.created_at - ctx.message.created_at).total_seconds()
-                        * 1000
+            before = time.monotonic()
+            message = await ctx.send(embed=emb)
+            ping = (time.monotonic() - before) * 1000
+            emb.colour = await ctx.embed_color()
+            emb.set_field_at(
+                1,
+                name="Message:",
+                value=box(
+                    str(
+                        int(
+                            (
+                                message.created_at - ctx.message.created_at
+                            ).total_seconds()
+                            * 1000
+                        )
                     )
-                )
-                + " ms",
-                "yaml",
-            ),
-        )
-        emb.set_field_at(2, name="Typing:", value=box(str(round(ping)) + " ms", "yaml"))
-        await message.edit(embed=emb)
+                    + " ms",
+                    "yaml",
+                ),
+            )
+            emb.set_field_at(
+                2, name="Typing:", value=box(str(round(ping)) + " ms", "yaml")
+            )
+            await message.edit(embed=emb)
+        else:
+            before = time.monotonic()
+            latency = int(round(self.bot.latency * 1000, 1))
+            message = await ctx.send("🏓 Pong")
+            ping = (time.monotonic() - before) * 1000
+            await message.edit(content=f"🏓 Discord WS: {latency}ms")
 
 
 def setup(bot):
