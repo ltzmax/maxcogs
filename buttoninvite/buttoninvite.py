1848e364373ca7d445aeb8fae2b6694c503dfbb6
Samuel <50765275+npc203@users.noreply.github.com>
2021-06-15 16:33:44 +0530

    [ButtonInvite] Added invmsg to buttoninvite to edit the invite message shown
    * added invmsg in buttoninvite
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * added invmsg to reset to default on no args
    
    Co-authored-by: npc203 <npc203@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- buttoninvite/buttoninvite.py
+++ buttoninvite/buttoninvite.py
@@ -1,6 +1,6 @@
 import discord
 from dislash.interactions import ActionRow, Button, ButtonStyle
-from redbot.core import commands
+from redbot.core import commands, Config
 
 old_invite = None
 
@@ -24,7 +24,21 @@ class ButtonInvite(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.config = Config.get_conf(self, identifier=12435434124)
+        self.def_msg = "Thank you for inviting {}\n\n**Click on the button!**"
+        self.config.register_global(msg=self.def_msg)
         self.bot.remove_command("invite")
+
+    @commands.is_owner()
+    @commands.command()
+    async def invmsg(self, ctx, *, message=None):
+        """Change the invite message shown in the embed"""
+        if message:
+            await self.config.msg.set(message)
+            await ctx.send(f"Sucessfully set the invite message")
+        else:
+            await self.config.msg.set(self.def_msg)
+            await ctx.send(f"Reset the invite message back to default")
 
     @commands.command()
     @commands.bot_has_permissions(embed_links=True)
@@ -40,7 +54,7 @@ class ButtonInvite(commands.Cog):
             title=f"{name}",
             colour=discord.Colour(0x5865F2),
             url=(ctx.bot.user.avatar_url_as(static_format="png")),
-            description=f"Thank you for inviting {name}\n\n**Click on the button!**",
+            description=(await self.config.msg()).format(name),
         )
         embed.set_thumbnail(url=ctx.bot.user.avatar_url_as(static_format="png"))
         embed.set_footer(text=f"Server count: {servers}")
