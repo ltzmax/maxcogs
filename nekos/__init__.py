4c3598f3405942cd15e42820037b38733cefee4b
MAX <63972751+maxbooiii@users.noreply.github.com>
2021-07-09 10:25:57 +0200

    [nekos] 0.2.0

---
+++ nekos/__init__.py
@@ -0,0 +1,24 @@
+from asyncio import create_task
+
+from .nekos import Nekos
+
+__red_end_user_data_statement__ = (
+    "This cog does not persistently store data or metadata about users."
+)
+
+# Logic from fixator10 https://github.com/fixator10/Fixator10-Cogs/blob/5b06d9f490e3dedada281ab60beb97626cb1a795/moreutils/__init__.py#L9
+async def setup_after_ready(bot):
+    await bot.wait_until_red_ready()
+    cog = Nekos(bot)
+    for name, command in cog.all_commands.items():
+        if not command.parent:
+            if bot.get_command(name):
+                command.name = f"n{command.name}"
+            for alias in command.aliases:
+                if bot.get_command(alias):
+                    command.aliases[command.aliases.index(alias)] = f"n{alias}"
+    bot.add_cog(cog)
+
+
+def setup(bot):
+    create_task(setup_after_ready(bot))
---
+++ nekos/nekos.py
@@ -0,0 +1,50 @@
+import aiohttp
+import discord
+from redbot.core import commands
+
+NEKOS_API = "https://nekos.best/"
+
+
+class Nekos(commands.Cog):
+    """Image cog that shows images."""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.session = aiohttp.ClientSession()
+
+    def cog_unload(self):
+        self.bot.loop.create_task(self.session.close())
+
+    __version__ = "0.2.0"
+    __author__ = "MAX"
+
+    def format_help_for_context(self, ctx: commands.Context) -> str:
+        """Thanks Sinbad!"""
+        pre_processed = super().format_help_for_context(ctx)
+        return f"{pre_processed}\n\nAuthor: {self.__author__}\nCog Version: {self.__version__}"
+
+    async def red_delete_data_for_user(self, **kwargs):
+        """Nothing to delete."""
+        return
+
+    @commands.command()
+    @commands.cooldown(1, 3, commands.BucketType.guild)
+    @commands.max_concurrency(1, commands.BucketType.guild)
+    @commands.bot_has_permissions(embed_links=True)
+    async def neko(self, ctx):
+        """Send a random neko images.
+
+        Powered by [nekos.best.](https://nekos.best)"""
+        async with aiohttp.ClientSession() as session:
+            async with session.get(NEKOS_API + "nekos") as response:
+                if response.status != 200:
+                    return await ctx.send(
+                        "Something went wrong while trying to contact API."
+                    )
+                url = await response.json()
+            embed = discord.Embed(
+                title="Here's a pic of neko", colour=await ctx.embed_color()
+            )
+            embed.set_footer(text="From nekos.best")
+            embed.set_image(url=url["url"])
+        await ctx.send(embed=embed)
