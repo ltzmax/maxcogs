d9a477fab3b7faf054af0610e90df7f9d078b8fa
MAX <63972751+maxbooiii@users.noreply.github.com>
2021-07-10 23:21:15 +0200

    [nekos] 0.3.0

--- nekos/nekos.py
+++ nekos/nekos.py
@@ -15,7 +15,7 @@ class Nekos(commands.Cog):
     def cog_unload(self):
         self.bot.loop.create_task(self.session.close())
 
-    __version__ = "0.2.0"
+    __version__ = "0.3.0"
     __author__ = "MAX"
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
