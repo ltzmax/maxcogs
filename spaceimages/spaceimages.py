865661c59b521572cdde45c4daa0723482d71f6e
MAX <63972751+maxbooiii@users.noreply.github.com>
2021-07-12 19:08:05 +0200

    [spaceimages] 0.2.0
    Cleanup and make it look more nice. it also felt more faster to get images and should be safe :tm: now.

--- spaceimages/spaceimages.py
+++ spaceimages/spaceimages.py
@@ -25,7 +25,7 @@ class SpaceImages(commands.Cog):
     def cog_unload(self):
         self.bot.loop.create_task(self.session.close())
 
-    __version__ = "0.1.0"
+    __version__ = "0.2.0"
     __author__ = "MAX"
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
@@ -38,11 +38,11 @@ class SpaceImages(commands.Cog):
         return
 
     @commands.command()
-    @commands.cooldown(1, 3, commands.BucketType.guild)
+    @commands.cooldown(1, 2, commands.BucketType.guild)
     @commands.max_concurrency(1, commands.BucketType.guild)
     @commands.bot_has_permissions(embed_links=True)
     async def space(self, ctx):
-        """Send a random space images."""
+        """Send a random space image."""
         await ctx.trigger_typing()
         async with aiohttp.ClientSession() as session:
             async with session.get(MARTINE_API + choice(SPACE)) as resp:
@@ -52,18 +52,19 @@ class SpaceImages(commands.Cog):
                     return await ctx.send(
                         "Something went wrong while trying to contact API."
                     )
-                response = await resp.json()
-            embed = discord.Embed(
-                title=response["data"].get("title", "[No Title]"),
-                url=response["data"]["post_url"],
-            )
-            embed.set_footer(
-                text=f"Powered by martinebot.com API | From r/{response['data']['subreddit']['name']}",
+                data = await resp.json()
+                title = data["data"].get("title")
+                subreddit = data["data"]["subreddit"]["name"]
+                images = data["data"]["image_url"]
+
+            emb = discord.Embed(title=f"{title}", url=f"{images}")
+            emb.set_footer(
+                text=f"Powered by martinebot.com API | From r/{subreddit}",
                 icon_url=MARTINE_ICON,
             )
-            embed.colour = await ctx.embed_color()
-            embed.set_image(url=response["data"]["image_url"])
+            emb.colour = await ctx.embed_color()
+            emb.set_image(url=images)
         try:
-            await ctx.send(embed=embed)
+            await ctx.send(embed=emb)
         except discord.HTTPException:
             await ctx.send("Something went wrong while posting an image.")
