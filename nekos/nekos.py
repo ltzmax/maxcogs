25850bcec4b14658d748408bd3fac2f00ce3e126
MAX <63972751+maxbooiii@users.noreply.github.com>
2021-08-25 17:23:17 +0200

    [nekos] 0.0.9

--- nekos/nekos.py
+++ nekos/nekos.py
@@ -3,6 +3,7 @@ import discord
 from redbot.core import commands
 
 NEKOS_API = "https://nekos.best/api/v1/"
+ICON = "https://cdn.discordapp.com/emojis/851544845956415488.png?v=1"
 
 
 class Nekos(commands.Cog):
@@ -15,7 +16,7 @@ class Nekos(commands.Cog):
     def cog_unload(self):
         self.bot.loop.create_task(self.session.close())
 
-    __version__ = "0.0.8"
+    __version__ = "0.0.9"
     __author__ = "MAX"
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
@@ -39,14 +40,22 @@ class Nekos(commands.Cog):
                     return await ctx.send(
                         "Something went wrong while trying to contact API."
                     )
+                if response.status == 502:
+                    return await ctx.send("Api is currently down, try again later.")
                 url = await response.json()
             emb = discord.Embed(
                 title="Here's a pic of neko",
                 description=f"Artist: [{url['artist_name']}]({url['artist_href']})\nSource: {url['source_url']}",
             )
             emb.colour = await ctx.embed_color()
-            emb.set_footer(text="Powered by nekos.best")
+            emb.set_footer(
+                text="Powered by nekos.best",
+                icon_url=ICON,
+            )
+        try:
             emb.set_image(url=url["url"])
+        except KeyError:
+            return await ctx.send("I ran into an issue. try again later.")
         try:
             await ctx.send(embed=emb)
         except discord.HTTPException:
