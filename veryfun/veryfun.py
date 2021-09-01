ae4d9e4cb06e6b7308a9ad397b17d132ee44cd44
MAX <63972751+maxbooiii@users.noreply.github.com>
2021-09-01 13:29:46 +0200

    [veryfun] 0.0.2

--- veryfun/veryfun.py
+++ veryfun/veryfun.py
@@ -20,7 +20,7 @@ class VeryFun(commands.Cog):
     def cog_unload(self):
         self.bot.loop.create_task(self.session.close())
 
-    __version__ = "0.0.1"
+    __version__ = "0.0.2"
     __author__ = "MAX"
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
