363afe425fbff252c9de80d14da0a549f82dd8a4
MAX <63972751+maxbooiii@users.noreply.github.com>
2021-10-10 08:44:40 +0200

    Settings to disable mentions.

--- pokenamegen/pokenamegen.py
+++ pokenamegen/pokenamegen.py
@@ -1,7 +1,7 @@
 import discord
 import logging
 from random import choice
-from redbot.core import commands
+from redbot.core import commands, Config
 
 from .constants import POKEMON
 
@@ -12,7 +12,7 @@ class PokeNameGen(commands.Cog):
     """Get random pokémon names."""
 
     __author__ = "MAX"
-    __version__ = "0.0.1"
+    __version__ = "0.0.2"
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
         """Thanks Sinbad!"""
@@ -25,10 +25,35 @@ class PokeNameGen(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.config = Config.get_conf(self, identifier=12435434124)
+        self.config.register_global(
+            mentions=True,
+        )
 
     # I cannot tell you how bored i was when i made this cog...
     # It's mostly just meant for people who like pokémon for the most.
     # Pokémon names are from the global national dex: https://pokemondb.net/pokedex/national.
+
+    @commands.is_owner()
+    @commands.group()
+    async def pset(self, ctx):
+        """Settings to change mentions to true or false."""
+
+    @pset.command(name="set", usage="<true_or_false>")
+    async def pset_set(self, ctx: commands.Context, mentions: bool):
+        """Change the mention to true or false.
+
+        Default is true.
+
+        **Example:**
+        - `[p]qset set false` this will disable the mentions on replies.
+
+        **Arguments:**
+        - `<true_or_false>` is where you set `true` or `false`."""
+
+        await self.config.mentions.set(mentions)
+        mentions = "true" if mentions else "false"
+        await ctx.send("Mentions is now {}.".format(mentions))
 
     @commands.command(aliases=["pokeng"])
     @commands.bot_has_permissions(embed_links=True)
@@ -42,7 +67,7 @@ class PokeNameGen(commands.Cog):
             colour=await ctx.embed_color(),
         )
         try:
-            await ctx.reply(embed=emb, mention_author=True)
+            await ctx.reply(embed=emb, mention_author=await self.config.mentions())
         except discord.HTTPException:
             await ctx.send(embed=emb)
             log.info(
