10da7d7e123bee54d35b764ef3ef1b4de990b8fd
MAX <63972751+maxbooiii@users.noreply.github.com>
2021-10-18 11:54:31 +0200

    [beg] Initial commit.

---
+++ beg/__init__.py
@@ -0,0 +1,5 @@
+from .beg import Beg
+
+
+def setup(bot):
+    bot.add_cog(Beg(bot))
---
+++ beg/constants.py
@@ -0,0 +1,22 @@
+BEG_WORDS = [
+    "Nice you got:",
+    "Someone felt nice and gave you:",
+    "What a lucky day, you got:",
+    "A woman passed by you and felt bad. So she gave you:",
+    "You looked like someone who needed more coins so you got:",
+    "The wait is over, you got:",
+    "Nice today you're lucky and got:",
+    "You robbed a bank and found:",
+    "You stole some coins from your mom and found:",
+    "Some friends gave you:",
+    "Your dad randomly gave you:",
+    "Someone was friendly so you got:",
+    "Your best friend wanted to give you birthday coins:",
+    "While walking to store, you found:",
+    "You beg for coins to your dad and got:",
+    "You beg for coins to your mom and got:",
+    "A man felt so sad for you so he gave you:",
+    "Today is your day and you got so lucky and got:",
+]
+
+# I need more words... honestly.
