73aa7ef26388b2f5a7c80b411551aaf3e2c1e913
MAX <63972751+maxbooiii@users.noreply.github.com>
2021-10-29 15:13:54 +0200

    :-1:

--- botstats/__init__.py
+++
@@ -1,9 +0,0 @@
-from .botstats import BotStats
-
-__red_end_user_data_statement__ = (
-    "This cog does not persistently store data or metadata about users."
-)
-
-
-def setup(bot):
-    bot.add_cog(BotStats(bot))
