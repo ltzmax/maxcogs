d83b2046d99ead64a8faa6fdc87c0dab9362d2b1
MAX <63972751+ltzmax@users.noreply.github.com>
2022-01-24 16:19:27 +0100

    [veryfun] 0.1.2

--- veryfun/veryfun.py
+++ veryfun/veryfun.py
@@ -14,9 +14,7 @@ class VeryFun(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(
-            self, identifier=0x345628097, force_registration=True
-        )
+        self.config = Config.get_conf(self, identifier=0x345628097)
         default_guild = {
             "mention": True,
         }
@@ -26,7 +24,7 @@ class VeryFun(commands.Cog):
     def cog_unload(self):
         self.bot.loop.create_task(self.session.close())
 
-    __version__ = "0.1.1"
+    __version__ = "0.1.2"
     __author__ = "MAX"
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
