4aae5dfd94b784b2186b92d9845e2d267f22ab78
MAX <63972751+ltzmax@users.noreply.github.com>
2022-02-06 09:43:34 +0100

    [stats] 0.0.2
    Making use of humanize_number, and added categories and bots count.

--- stats/stats.py
+++ stats/stats.py
@@ -2,6 +2,7 @@ import datetime
 
 import discord
 from redbot.core import commands
+from redbot.core.utils.chat_formatting import humanize_number
 
 
 class Stats(commands.Cog):
@@ -12,7 +13,7 @@ class Stats(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    __version__ = "0.0.1"
+    __version__ = "0.0.2"
     __author__ = "MAX"
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
@@ -24,23 +25,29 @@ class Stats(commands.Cog):
         """Nothing to delete."""
         return
 
-    @commands.command()
+    @commands.command(aliases=["ssinfo", "statics"])
     @commands.bot_has_permissions(embed_links=True)
     async def statsinfo(self, ctx):
         """Shows stats about [botname].
 
         This shows the total for all servers your bot is in."""
-        voice_channels = 0
         text_channels = 0
-        stage_channels = 0
+        voice_channels = 0
+        categories = 0
         emojis = 0
-        # This is the simplest thing i could do to get gobal.
+        stage_channels = 0
+        users = 0
+        bots = 0
         for guild in self.bot.guilds:
-            voice_channels += len(guild.voice_channels)
+            users += len(guild.members)
+            bots += len([m for m in guild.members if m.bot])
             text_channels += len(guild.text_channels)
+            voice_channels += len(guild.voice_channels)
             stage_channels += len(guild.stage_channels)
+            categories += len(guild.categories)
             emojis += len(guild.emojis)
 
+        guilds = len(self.bot.guilds)
         uptime = self.bot.uptime.replace(tzinfo=datetime.timezone.utc)
 
         # this needs to change in dpy 2.0
@@ -48,16 +55,27 @@ class Stats(commands.Cog):
             avatar = self.bot.user.avatar_url
 
         embed = discord.Embed(
-            title="Stats for {}".format(self.bot.user.name),
-            description="Here are the stats for {}".format(self.bot.user.name),
+            title="BotStats for {}".format(self.bot.user.name),
             colour=await ctx.embed_colour(),
         )
         embed.set_thumbnail(url=avatar)
-        embed.add_field(name="Servers:", value=len(self.bot.guilds))
-        embed.add_field(name="Users:", value=len(self.bot.users))
-        embed.add_field(name="Text Channels:", value=text_channels)
-        embed.add_field(name="Voice channels:", value=voice_channels)
-        embed.add_field(name="Stage channels:", value=stage_channels)
-        embed.add_field(name="Emojis:", value=emojis)
-        embed.add_field(name="Been up since:", value=f"<t:{int(uptime.timestamp())}:f>")
+        embed.add_field(name="Servers:", value=humanize_number(guilds), inline=True)
+        embed.add_field(name="Users:", value=humanize_number(users), inline=True)
+        embed.add_field(name="Bots:", value=humanize_number(bots), inline=True)
+        embed.add_field(
+            name="Text Channels:", value=humanize_number(text_channels), inline=True
+        )
+        embed.add_field(
+            name="Voice Channels:", value=humanize_number(voice_channels), inline=True
+        )
+        embed.add_field(
+            name="Stage Channels:", value=humanize_number(stage_channels), inline=True
+        )
+        embed.add_field(
+            name="Categories:", value=humanize_number(categories), inline=True
+        )
+        embed.add_field(name="Emojis:", value=humanize_number(emojis), inline=True)
+        embed.add_field(
+            name="Been online since:", value=f"<t:{int(uptime.timestamp())}:f>"
+        )
         await ctx.send(embed=embed)
