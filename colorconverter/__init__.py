e9972b2c95cd80e49c840caf834466b92b00eb3c
MAX <63972751+ltzmax@users.noreply.github.com>
2022-02-14 19:19:13 +0100

    This to be removed.

--- colorconverter/__init__.py
+++
@@ -1,9 +0,0 @@
-from .colorconverter import ColorConverter
-
-__red_end_user_data_statement__ = (
-    "This cog does not persistently store data about users."
-)
-
-
-def setup(bot):
-    bot.add_cog(ColorConverter(bot))
