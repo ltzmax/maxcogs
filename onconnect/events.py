f9876f0f522ebdb339842a93ad19a3c658836295
MAX <63972751+ltzmax@users.noreply.github.com>
2022-07-14 14:38:45 +0200

    this may work

--- onconnect/events.py
+++ onconnect/events.py
@@ -37,31 +37,35 @@ class Events(MixinMeta):
     @commands.Cog.listener()
     async def on_shard_connect(self, shard_id: int) -> None:
         emoji = await self.config.orange()
-        shard_count = self.bot.shard_count
-        message = f"{emoji} Shard #{shard_id}/{shard_count} connected!"
+        for s in self.bot.shards:
+            shard_count = self.bot.shard_count
+        message = f"{emoji} Shard #{s+1}/{shard_count} connected!"
         await self.bot.wait_until_red_ready()
         await self.send_event_message(message=message, colour=discord.Colour.orange())
 
     @commands.Cog.listener()
     async def on_shard_ready(self, shard_id: int) -> None:
         emoji = await self.config.green()
-        shard_count = self.bot.shard_count
-        message = f"{emoji} Shard #{shard_id}/{shard_count} ready!"
+        for s in self.bot.shards:
+            shard_count = self.bot.shard_count
+        message = f"{emoji} Shard #{s+1}/{shard_count} ready!"
         await self.bot.wait_until_red_ready()
         await self.send_event_message(message=message, colour=discord.Colour.green())
 
     @commands.Cog.listener()
     async def on_shard_disconnect(self, shard_id: int) -> None:
         emoji = await self.config.red()
-        shard_count = self.bot.shard_count
-        message = f"{emoji} Shard #{shard_id}/{shard_count} disconnected!"
+        for s in self.bot.shards:
+            shard_count = self.bot.shard_count
+        message = f"{emoji} Shard #{s+1}/{shard_count} disconnected!"
         await self.send_event_message(message=message, colour=discord.Colour.red())
 
     @commands.Cog.listener()
     async def on_shard_resumed(self, shard_id: int) -> None:
         emoji = await self.config.orange()
-        shard_count = self.bot.shard_count
-        message = f"{emoji} Shard #{shard_id}/{shard_count} resumed!"
+        for s in self.bot.shards:
+            shard_count = self.bot.shard_count
+        message = f"{emoji} Shard #{s+1}/{shard_count} resumed!"
         await self.bot.wait_until_red_ready()
         await self.send_event_message(message=message, colour=discord.Colour.orange())
 
