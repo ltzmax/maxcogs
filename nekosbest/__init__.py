391ed55591942e6c90a07b2d117a47e58d14cb7f
MAX <63972751+ltzmax@users.noreply.github.com>
2022-08-05 14:21:04 +0200

    ops

--- nekosbest/__init__.py
+++ nekosbest/__init__.py
@@ -1,4 +1,4 @@
-from .nekosbest import Nekosbest
+from .nekosbest import NekosBest
 
 __red_end_user_data_statement__ = (
     "This cog does not persistently store data about users."
@@ -6,4 +6,4 @@ __red_end_user_data_statement__ = (
 
 
 def setup(bot):
-    bot.add_cog(Nekosbest(bot))
+    bot.add_cog(NekosBest(bot))
