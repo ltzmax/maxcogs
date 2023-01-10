59d3a422563dbac9a4bb3fe317477dd336485680
MAX <63972751+ltzmax@users.noreply.github.com>
2023-01-10 19:01:47 +0100

    License

--- away/away.py
+++ away/away.py
@@ -1,8 +1,30 @@
 # This cog was orginially made by TheDiscordHistorian (kato#0666) and dia ♡#0666.
 # And are now maintained by ltzmax.
 # Most people knew this cog was without a license on the original repo source
-# but permission to continue the cog was granted by dia #0666.
-# Please continue to respect the original authors to not steal the code without permissions.
+# but permission to continue the cog was granted by dia #0666 to add it under MIT.
+"""
+MIT License
+
+Copyright (c) 2022-present ltzmax
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 import logging
 from typing import Literal
 
