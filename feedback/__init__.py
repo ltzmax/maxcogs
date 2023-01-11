cd362a014fa19387cdb09ca6ac3f83a3284d66c6
MAX <63972751+ltzmax@users.noreply.github.com>
2023-01-11 13:53:12 +0100

    Doesnt really store data.

--- feedback/__init__.py
+++ feedback/__init__.py
@@ -1,7 +1,8 @@
 from .feedback import Feedback
 
-__red_end_user_data_statement__ = "This cog does store data by users who use the feedback command for redisplaying their discord ID and username. This does not store any data that was not provided by the feedback command."
-
+__red_end_user_data_statement__ = (
+    "This cog does not persistently store data about users."
+)
 
 async def setup(bot):
     await bot.add_cog(Feedback(bot))
