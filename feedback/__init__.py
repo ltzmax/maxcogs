87f721d684f0444631570b9da166692268a9dfc9
MAX <63972751+ltzmax@users.noreply.github.com>
2023-01-13 19:22:19 +0100

    It turned into a mess.

--- feedback/__init__.py
+++
@@ -1,9 +0,0 @@
-from .feedback import Feedback
-
-__red_end_user_data_statement__ = (
-    "This cog does not persistently store data about users."
-)
-
-
-async def setup(bot):
-    await bot.add_cog(Feedback(bot))
