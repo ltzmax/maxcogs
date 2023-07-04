0311ca3cc5f524e1b8fb6c7fc18ea5bafe76c95e
Lemon Rose <yash.kul69@gmail.com>
2023-07-04 19:39:13 +0530

    [DailyEconomy] type hint cog + little refactor

--- dailyeconomy/__init__.py
+++ dailyeconomy/__init__.py
@@ -3,5 +3,6 @@ from redbot.core.bot import Red
 from .dailyeconomy import DailyEconomy
 
 
-async def setup(bot: Red):
-    await bot.add_cog(DailyEconomy(bot))
+async def setup(bot: Red) -> None:
+    cog = DailyEconomy(bot)
+    await bot.add_cog(cog)
