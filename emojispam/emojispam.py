307b405c38b1df3003abc38f55852f595b97bca5
MAX <63972751+ltzmax@users.noreply.github.com>
2023-10-14 16:10:11 +0200

    Revert. cause it didnt work >_>

--- emojispam/emojispam.py
+++ emojispam/emojispam.py
@@ -34,22 +34,7 @@ from redbot.core.utils.views import ConfirmView
 log: RedTraceLogger = getLogger("red.maxcogs.emojispam")
 
 EMOJI_REGEX = re.compile(
-    "["
-    "<a?:(\w+):(\d+)>" # custom emoji and animated on discord
-    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
-    "\U0001F1E6-\U0001F1FF"  # flags (Android)
-    "\U0001F300-\U0001F5FF"  # symbols & pictographs
-    "\U0001F600-\U0001F64F"  # emoticons
-    "\U0001F680-\U0001F6FF"  # transport & map symbols
-    "\U0001F700-\U0001F77F"  # alchemical symbols
-    "\U0001F780-\U0001F7FF"  # Geometric Shapes Extended
-    "\U0001F800-\U0001F8FF"  # Supplemental Arrows-C
-    "\U0001F900-\U0001F9FF"  # Supplemental Symbols and Pictographs
-    "\U0001FA00-\U0001FA6F"  # Chess Symbols
-    "\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
-    "\U00002702-\U000027B0"  # Dingbats
-    "\U000024C2-\U0001F251"  # enclosed characters
-    "]+"
+    "<a?:(\w+):(\d+)>|[\U0001F000-\U0001F6FF]|[\U0001F900-\U0001F9FF]|[\U0001F600-\U0001F64F]|[\U0001F680-\U0001F6FF]|[\U0001F1E0-\U0001F1FF]|[\U0001F1E6-\U0001F1FF]{2}|[\U0001F3F3-\U0001F3FF][\U000E0067-\U000E007F]{2}|[\U0001F3F4-\U0001F3FF][\U000E0067-\U000E007F]{2}|[\U0001F3F9-\U0001F3FA][\U000E0067-\U000E007F]{2}|[\U0001F3FB-\U0001F3FF][\U000E0067-\U000E007F]{2}|[\U0001F1E6-\U0001F1FF][\U0001F1E6-\U0001F1FF][\U0001F1E6-\U0001F1FF][\U0001F1E6-\U0001F1FF]|\u2B1C|\u2B1B|[\U0001F7E2-\U0001F7E8]|[\U0001F170-\U0001F19A]"
 )
 
 class EmojiSpam(commands.Cog):
