7109a048c5b1c09d02dde4234895b6f9d1122928
Lemon Rose <yash.kul69@gmail.com>
2023-10-15 17:50:17 +0530

    [NoSpoiler] add dependency checker

--- nospoiler/__init__.py
+++ nospoiler/__init__.py
@@ -1,6 +1,30 @@
+import json
+import re
+from pathlib import Path
+from typing import Match, Optional, Pattern
+
 from redbot.core.bot import Red
+from redbot.core.errors import CogLoadError
 
 from .nospoiler import NoSpoiler
+from ._tagscript import validate_tagscriptengine
+
+VERSION_RE: Pattern[str] = re.compile(r"TagScript==(\d\.\d\.\d)")
+
+with open(Path(__file__).parent / "info.json") as f:
+    data = json.load(f)
+
+tse_version = None
+for requirement in data.get("requirements", []):
+    match: Optional[Match[str]] = VERSION_RE.search(requirement)
+    if match:
+        tse_version = match.group(1)
+        break
+
+if not tse_version:
+    raise CogLoadError(
+        "Failed to find TagScriptEngine version number. Please report this to the cog author."
+    )
 
 __red_end_user_data_statement__ = (
     "This cog does not persistently store data about users."
@@ -8,5 +32,6 @@ __red_end_user_data_statement__ = (
 
 
 async def setup(bot: Red) -> None:
+    await validate_tagscriptengine(bot, tse_version)
     cog = NoSpoiler(bot)
     await bot.add_cog(cog)
