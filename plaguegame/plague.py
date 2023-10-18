ac28a4f2aca1b498ddae7e8cc6771482088194c7
MAX <63972751+ltzmax@users.noreply.github.com>
2023-10-18 21:42:08 +0200

    Add slash support  for `plagueprofile`.

--- plaguegame/plague.py
+++ plaguegame/plague.py
@@ -28,7 +28,7 @@ import random
 from collections import Counter
 
 import discord
-from redbot.core import Config, bank, checks, commands
+from redbot.core import Config, bank, commands, app_commands
 from redbot.core.utils import AsyncIter
 from redbot.core.utils.chat_formatting import (
     humanize_number,
@@ -152,9 +152,10 @@ class Plague(commands.Cog):
         result = await self.cure_user(ctx=ctx, user=member)
         await ctx.send(result)
 
-    @checks.bot_has_permissions(embed_links=True)
+    @commands.bot_has_permissions(embed_links=True)
     @commands.guild_only()
-    @commands.command("plagueprofile", aliases=["pprofile"])
+    @app_commands.describe(member=("The member you want to check."))
+    @commands.hybrid_command("plagueprofile", aliases=["pprofile"])
     async def plagueProfile(self, ctx, *, member: FuzzyHuman = None):
         """Show's your Plague Game profile"""
         member = member or ctx.author
