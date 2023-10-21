6ed76b1df93f4d44a1841c6274c4c49c97915e68
MAX <63972751+ltzmax@users.noreply.github.com>
2023-10-21 19:00:24 +0200

    Add `Generation` too into the utils.
    Bot restart is required after update utils.

--- whosthatpokemon/whosthatpokemon.py
+++ whosthatpokemon/whosthatpokemon.py
@@ -37,10 +37,9 @@ from redbot.core import Config, app_commands, commands, errors
 from redbot.core.bot import Red
 from redbot.core.utils.chat_formatting import box, humanize_list, humanize_number
 from redbot.core.utils.views import ConfirmView, SimpleMenu
-from .converter import Generation
 
 try:
-    from maxcogs_utils.wtpview import WhosThatPokemonView, generate_image, get_data
+    from maxcogs_utils.wtpview import WhosThatPokemonView, generate_image, get_data, Generation
 except ModuleNotFoundError:
     raise errors.CogLoadError(
         "You need to install maxcogs-utils to use this cog.\n"
