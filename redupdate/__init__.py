ebdae03d1fbb6fc701067c002960a4591fc20c7d
ltzmax <ltzmax@users.noreply.github.com>
2023-10-26 11:41:44 +0000

    Format Python code with psf/black

--- redupdate/__init__.py
+++ redupdate/__init__.py
@@ -10,6 +10,8 @@ __red_end_user_data_statement__ = (
 
 async def setup(bot: Red) -> None:
     if not bot._cli_flags.dev or not bot.get_cog("Shell"):
-        raise errors.CogLoadError("This cog is only available with dev flag enabled.\nYou also need to have the Shell cog from [JackCogs](<https://github.com/Jackenmen/JackCogs>) installed and loaded.")
+        raise errors.CogLoadError(
+            "This cog is only available with dev flag enabled.\nYou also need to have the Shell cog from [JackCogs](<https://github.com/Jackenmen/JackCogs>) installed and loaded."
+        )
     cog = RedUpdate(bot)
     await bot.add_cog(cog)
