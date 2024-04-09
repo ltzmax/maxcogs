b94d38cb417e74b3b3d8ea1f9a608128cdc7f2a3
MAX <63972751+ltzmax@users.noreply.github.com>
2024-04-09 12:15:25 +0200

    Missing import

--- capspam/core.py
+++ capspam/core.py
@@ -22,6 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+import io
 import logging
 import re
 from typing import Dict, Literal, Optional, Union
