e384bb3812ceb04a95d36bf08a5bb87da36cbab5
MAX <63972751+ltzmax@users.noreply.github.com>
2024-08-24 01:21:32 +0200

    Ops

--- nekosbest/nekosbest.py
+++ nekosbest/nekosbest.py
@@ -138,6 +138,7 @@ class NekosBest(commands.Cog):
         view = ConfirmView(ctx.author, disable_buttons=True)
         view.message = await ctx.send("Are you sure you want to reset everything?", view=view)
         await view.wait()
+        if view.result:
             await self.config.clear_all_users()
             await ctx.send("All command counts have been reset.")
         else:
