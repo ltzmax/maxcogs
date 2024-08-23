0a3d60fb1ee5bcbdc3a67aaaf9d3d6e0ccbd066f
MAX <63972751+ltzmax@users.noreply.github.com>
2024-08-24 01:17:48 +0200

    Remove unused import.

--- pokemon/pokemon.py
+++ pokemon/pokemon.py
@@ -40,7 +40,7 @@ from redbot.core.data_manager import bundled_data_path
 from redbot.core import Config, app_commands, commands
 from redbot.core.bot import Red
 from redbot.core.utils.chat_formatting import box, humanize_list, humanize_number
-from redbot.core.utils.views import ConfirmView, SimpleMenu
+from redbot.core.utils.views import SimpleMenu
 from .converters import (
     WhosThatPokemonView,
     get_data,
