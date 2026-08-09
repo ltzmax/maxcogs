e18654cd5b9e492a4197238e7ac914a27e01701b
MAX <63972751+ltzmax@users.noreply.github.com>
2026-08-09 12:46:29 +0200

    Remove NBA cog
    i dont find a newly updated api and nba_api doesnt keep updates anymore so its hard to keep going.

--- nba/__init__.py
+++
@@ -1,9 +0,0 @@
-from .nba import NBA
-
-
-__red_end_user_data_statement__ = "This cog does not persistently store data about users."
-
-
-async def setup(bot):
-    cog = NBA(bot)
-    await bot.add_cog(cog)
